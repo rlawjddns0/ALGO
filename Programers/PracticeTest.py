579ce56a17c1e0f4bcdbe08edd71c6a5c62f826c
KIMJEONGWOON <rlawjddns0@naver.com>
2021-01-11 00:44:19 +0900

    프로그래머스 연습

---
+++ Programers/PracticeTest.py
@@ -0,0 +1,23 @@
+from itertools import cycle
+def solution(answers):
+    scores = [0, 0, 0]
+    answer = []
+    pattern1 = range(1, 6)
+    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    for i in range(len(answers)):
+        print(pattern1[i % len(pattern1)])
+        if answers[i] == pattern1[i % len(pattern1)]:
+            scores[0] += 1
+        if answers[i] == pattern2[i % len(pattern2)]:
+            scores[1] += 1
+        if answers[i] == pattern3[i % len(pattern3)]:##순환 주기가 다르기 때문에 순환주기로 나눠준다
+            scores[2] += 1
+
+    max_score = max(scores)
+    for i in range(3):
+        if scores[i] == max_score:
+            answer.append(i + 1)
+
+    return answer
